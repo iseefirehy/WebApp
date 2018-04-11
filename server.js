60fda1a8fb0873951fdb941141711d3b6a028e7d
iseefirehy_hongyu <zhanghongyu2011@gmail.com>
2018-04-11 11:33:42 -0700

    Second commit

--- server.js
+++ server.js
@@ -4,20 +4,36 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
 app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+
+    console.log(`${now}:`);
+    next();
+});
+
+
+
+hbs.registerHelper('getCurrentYear',() =>{
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
 app.get('/',(req,res) => {
    res.render('home.hbs',{
        pageTitle: 'Home Page',
-       welcomeMessage: 'Welcome to my website',
-       currentYear: new Date().getFullYear()
+       welcomeMessage: 'Welcome to my website'
+
    });
 
 });
 app.get('/about',(req,res) =>{
     res.render('about.hbs',{
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 app.listen(3000, ()=>{
