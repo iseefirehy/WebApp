718838eb7973ab04ed01de99e62d3f0bff203b03
iseefirehy_hongyu <zhanghongyu2011@gmail.com>
2018-04-11 15:09:01 -0700

    Second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express  = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 
 var app = express();
@@ -11,7 +12,13 @@ app.use(express.static(__dirname + '/public'));
 app.use((req,res,next)=>{
     var now = new Date().toString();
 
-    console.log(`${now}: ${req.method} ${req.url}`);
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err) =>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
     next();
 });
 
